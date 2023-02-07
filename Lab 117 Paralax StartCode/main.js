653820ae4091b92418acca0815ec22fdc716ffb3
Malcolm McDonald <810828@seq.org>
2023-02-07 15:36:04 -0800

    adding a pannel to the left of the canvas

--- Lab 117 Paralax StartCode/main.js
+++ Lab 117 Paralax StartCode/main.js
@@ -4,11 +4,12 @@
 var game, canvas, ctx;
 window.onload = init;//  After the window has been loaded, go to init
 
-function init(){
+function init() {
   canvas = document.createElement('canvas');
   canvas.style.border = 'solid black 2px';
   canvas.style.backgroundColor = 'rgba(0,0,0, .95)';
-  canvas.width = 1096;  // 800 - 4 for the border
+  canvas.style.float = "right"
+  canvas.width = 996;  // 800 - 4 for the border
   canvas.height = 696; // 700 - 4 for the border
   ctx = canvas.getContext('2d'); // This is the context
   game = new Game();
@@ -16,11 +17,11 @@ function init(){
 }
 
 
-function animate(){
+function animate() {
   ctx.fillStyle = 'rgba(0,0,0,.05)'
-  ctx.fillRect(0,0,canvas.width,canvas.height); 
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   game.update()
   requestAnimationFrame(animate);
 }
 
-  
+
